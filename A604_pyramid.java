721ee0d8aba614d3dd5a89f0a0432cd2876dc2b7
Murad <muradrustamov@gmail.com>
2019-11-25 03:17:28 +0100

    File A604 added to homework repository [Commit was Autogenerated by Shell script]

---
+++ A604_pyramid.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class A04_pyramid {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Geben Sie die Höhe der Pyramide ein : ");
+		int height = sc.nextInt();
+		int cnt = 0;
+		for(int y = 0; y < height; ++y) {
+			for(int x = 0; x < height*2; ++x) {
+				if(x >= height-cnt && x <= height+cnt)
+					System.out.print("X");
+				else
+					System.out.print(" ");
+			}
+			++cnt;
+			System.out.println();
+		}
+	}
+}
