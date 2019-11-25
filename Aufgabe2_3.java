fb7e7bfc104c052cbe44bc565bf4f7c56d7a1e7f
Murad <muradrustamov@gmail.com>
2019-11-25 03:09:48 +0100

    File A05 added to homework repository [Commit was Autogenerated by Shell script]

--- Aufgabe2_3.java
+++
@@ -1,9 +0,0 @@
-public class DeMorgan {
-	public static void main(String[] args) {
-		boolean a = true;
-		boolean b = true;
-
-		System.out.println(!(a && b) == !a || !b);
-		System.out.println(!(a || b) == !a && !b);
-	}
-}
--- World.java
+++
@@ -1,5 +0,0 @@
-public class World {
-	public static void main(String[] args) {
-		
-	}
-}
