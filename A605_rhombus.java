93b505d1696c2ccad3ee6dc24539718652289d4c
Murad <muradrustamov@gmail.com>
2019-11-25 17:30:11 +0100

    File A605_rhombus added to homework repository [Commit was Autogenerated by Shell script]

--- A605_rhombus.java
+++ A605_rhombus.java
@@ -2,23 +2,21 @@ import java.util.Scanner;
 public class A605_rhombus{
 	public static void draw(int height) {
 		int step = 0;
-		String ups = "";
-		boolean even = height % 2 == 0;
-		height = even ? ++height : height;
 		int center = height/2;
 		for(int y = 0; y < height; ++y) {
 			for(int x = 0; x < height; ++x) {
-				if(x >= center-step && x <= center+step && !(y == center && x == center && even)) {
+				if(x >= center-step && x <= center+step) {
 					System.out.print("X");
 				} else {
 					System.out.print(" ");
 				}
 			}
 			System.out.println();
-			if(y < center) 
-				++step;
-			else
-				--step;
+			if(y < center) {
+				step+= (height % 2 == 0 && y == center-1) ? 0 : 1;
+		       	} else {
+				--step; 
+			}
 		}
 	}
 
